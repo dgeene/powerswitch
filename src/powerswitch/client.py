01da040b801095e5325ff4dfc6a4048be8df8db2
dgeene <david.geene@gmail.com>
2025-08-21 07:26:19 -0400

    move http auth code to httpclient

--- src/powerswitch/client.py
+++ src/powerswitch/client.py
@@ -1,4 +1,6 @@
+import hashlib
 import requests
+from bs4 import BeautifulSoup
 from requests.auth import HTTPDigestAuth
 from abc import ABC, abstractmethod
 
@@ -74,6 +76,46 @@ class HTTPClient(PowerSwitchClient):
     - Turn on:  GET /relay/outlet=?{switch_id}=ON
     - Turn off: GET /relay/outlet=?{switch_id}=OFF
     """
+
+    def __init__(self, address, username, password):
+        super().__init__(address, username, password)
+        self.secure_login = False
+        self.__session = requests.Session()
+        self.__session.auth = HTTPDigestAuth(username, password)
+        self.__authenticate()
+
+    def __authenticate(self):
+        res = self.__session.get(f'http://{self.address}/', allow_redirects=False)
+        print(res.is_redirect)
+        print(res.headers)
+        print(res.content)
+        print(self.__session.cookies)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        fields = {}
+        for field in soup.find_all('input'):
+            name = field.get('name', None)
+            value = field.get('value', '')
+            if name:
+                fields[name] = value
+        print(fields)
+        fields['Username'] = self.username
+        fields['Password'] = self.password
+        form_response = fields['Challenge'] + fields['Username'] + fields['Password'] + fields['Challenge']
+        m = hashlib.md5()
+        m.update(form_response.encode())
+        data = {'Username': self.username, 'Password': m.hexdigest()}
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        try:
+            response = self.__session.post(f'http://{self.address}/login.tgi', headers=headers, data=data, timeout=2.0, verify=False)
+        except requests.exceptions.ConnectTimeout:
+            self.secure_login = False
+            self.__session = None
+            return
+        print(self.__session.cookies)
+        if response.status_code == 200:
+            if 'Set-Cookie' in response.headers:
+                self.secure_login = True
+
     def turn_on(self, switch_id: int):
         """Turns on a switch."""
         # Example implementation:
@@ -94,4 +136,6 @@ class HTTPClient(PowerSwitchClient):
 
     def outlets(self):
         """Get all outlets"""
-        pass
+        res = self.__session.get(f'http://{self.address}/index.htm', timeout=2.0, verify=False, allow_redirects=True)
+        print(res.status_code)
+        return res.content
--- tests/test_rest_client.py
+++ tests/test_rest_client.py
@@ -1,8 +1,14 @@
-from src.powerswitch.client import RestClient
+from src.powerswitch.client import RestClient, HTTPClient
 
 class TestRestClient:
 
     def test_client_turn_on(self):
         switch = RestClient('192.168.0.100', 'admin', '1234')
         switch.turn_on(1)
+        print(switch.outlets())
+
+class TestHttpClient:
+
+    def test_client_turn_on(self):
+        switch = HTTPClient('192.168.0.100', 'admin', '1234')
         print(switch.outlets())
